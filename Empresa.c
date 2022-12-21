8bedfb7107c802096e9777b305df755e5ac4599d
JP-NN <113746635+JP-NN@users.noreply.github.com>
2022-12-21 10:22:35 -0600

    Segunda linea de prueba

--- Empresa.c
+++ Empresa.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
 int main(){
-    printf("EL dia de hoy la empresa entra en desarrollo");
+    printf("EL dia de hoy la empresa entra en desarrollo.");
+    printf("Segunda linea de prueba");
 }
