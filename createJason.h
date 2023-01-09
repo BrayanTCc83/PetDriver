b17a3480a0d4f3e6e2812c0fe18a74414991269d
Brandom Martinez <90359711+bSkock@users.noreply.github.com>
2023-01-08 20:41:58 -0600

    cebecera

---
+++ createJason.h
@@ -0,0 +1,25 @@
+#ifndef JASON
+#define JASON
+
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct Empleado
+{
+    int noIdenticacion;
+    char nombre[32];
+    int noViajes;
+    float tarifa;
+
+}Empleado;
+
+typedef struct Empresa
+{
+    char nameEmpresa[40];
+    char direccion[100];
+    Empleado empleado[5];
+}Empresa;
+
+
+void create_json(Empresa* empresa, char* file_name);
+
+#endif
