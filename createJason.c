0492ceb22c2403b5bb3663c6a6d7ac8861123355
Brandom Martinez <90359711+bSkock@users.noreply.github.com>
2023-01-08 20:40:33 -0600

    jason para los regitros
    ya funcionaaaaa

---
+++ createJason.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "hashTable.h"
+#include "createJason.h"
+
+
+void create_json(Empresa* empresa, char* file_name){
+
+    size_t i;
+    strcat(file_name, ".json");
+    FILE* json_output = fopen(file_name, "w");
+    if (json_output == NULL){
+        printf( "El archivo %s no pudo ser abierto para escritura.\n", file_name);
+        exit( -1 );
+    }
+    fprintf(json_output, "{\n");
+    fprintf(json_output, "\"nombre de la empresa\": \"%s\",\n", empresa->nameEmpresa);
+    fprintf(json_output, "\"direccion\": \"%s\",\n", empresa->direccion);
+    fprintf(json_output, "\"empleados\": [\n");
+    for(i = 0; i < 5; i++){
+        if (i > 0)
+        fprintf(json_output, ",\n");
+        fprintf(json_output, "{ ");
+        fprintf(json_output, "\"trabajador\": \"%s\", \"id\": %d, \"no. viajes\": %d,\"tarifa\": %.2f", empresa->empleado[i].nombre,
+                                                                                                        empresa->empleado[i].noIdenticacion,
+                                                                                                        empresa->empleado[i].noViajes,
+                                                                                                        empresa->empleado[i].tarifa);
+        fprintf(json_output, " }");
+    }
+    fprintf(json_output, "\n]"); 
+    fprintf(json_output, "\n}");
+    fclose(json_output);
+ 
+}
+/*
+Para pruebas
+int main(int argc, char const *argv[])
+{
+       Empresa petDriver={
+      .direccion="calle",
+      .nameEmpresa="Pet Driver",
+      .empleado [0]={ 1010,     "Brandom",       1,         9.0 }, 
+      .empleado [1]={ 2021,     "Guillermo",     2,        16.5 },
+      .empleado [2]={ 3032,     "Brayan",        3,        28.5 }, 
+      .empleado [3]={ 4043,     "Emanuel",       4,        14.0 },
+      .empleado [4]={ 5054,     "Juan Pablo",    5,        25.0 }
+
+   };
+    char nombreArchivoJson[80] = "test";
+   create_json(&petDriver,nombreArchivoJson);
+    
+}*/
