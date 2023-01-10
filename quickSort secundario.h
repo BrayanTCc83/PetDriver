14e4e672324370092cf6617ae46940f5e84f3bfb
Brandom Martinez <90359711+bSkock@users.noreply.github.com>
2023-01-10 10:08:02 -0600

    header quick

--- quickSort secundario.h
+++ quickSort secundario.h
@@ -1,7 +1,17 @@
-#ifndef quickSort
-#define quickSort
+#ifndef QUICKSORT
+#define QUICKSORT
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "createJason.h"
+#include <stdbool.h>
+
+#define MAX_TRABAJADORES 5
+
+bool Product_IsLower_By_ID(Empleado *p1, Empleado *p2);
+bool Product_IsGreater_By_ID(Empleado *p1, Empleado *p2);
+void quicksort(Empresa empresa);
+void QuickSort(Empresa *empresa, int first, int last);
+void swap(Empleado *x1,Empleado *x2);
 
 #endif
-
-
-
