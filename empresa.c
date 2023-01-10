4c4d26fb7a248f4f4cb8695eaa1dfefa09f3ca65
Brandom Martinez <90359711+bSkock@users.noreply.github.com>
2023-01-10 10:16:26 -0600

    Union empleados

--- empresa.c
+++ empresa.c
@@ -8,7 +8,7 @@
 
 /*
 * Para ejecutar el programa
-* gcc -o empresa.exe empresa.c hash_Table.c
+* gcc -o empresa.exe empresa.c hash_Table.c createJason.c
 */
 
 #define HASH_TABLE_SIZE 7
@@ -16,6 +16,7 @@
 #define MAX_DATA 10
 int main()
 {
+
       Empresa petDriver={
       .direccion="Escolar 04360, C.U., Coyoacán, 04510 Ciudad de México, CDMX",
       .nameEmpresa="Pet Driver",
@@ -26,33 +27,20 @@ int main()
       .empleados [4]={ 5054,     "Juan Pablo",    5,        25.0 }
 
    };
-   Empleado empleados[ MAX_TRABAJADORES ] =
-   {
-      // id        Nombre      Num_viajes  tarifa         idx
-      { 1010,     "Brandom",       1,         9.0 },    // [0]
-      { 2021,     "Guillermo",     2,        16.5 },    // [1]
-      { 3032,     "Brayan",        3,        28.5 },    // ...
-      { 4043,     "Emanuel",       4,        14.0 },
-      { 5054,     "Juan Pablo",    5,        25.0 },
-   };
-
-   for(int i=0;i<MAX_TRABAJADORES;i++){
-      empresa.empleados[i]=empleados[i];
-   }
 
    Hash_table* tabla = HT_New( HASH_TABLE_SIZE );
    assert( tabla ); // el programa se detiene si la tabla no se pudo crear
    assert( HT_IsEmpty( tabla ) == true );// la tabla recién se creó, debe estar vacía
 
    for(int i=0; i < MAX_TRABAJADORES && !HT_IsFull( tabla ); ++i ){
-      HT_Insert( tabla,empleados[ i ].id,i);
+      HT_Insert( tabla,petDriver.empleados[i].id,i);
    }
 
-char archivo[32]="nombre del archivo";
+   
 
    //quickSort(empleados)
-   create_json(&empresa,archivo);
-
+   char archivo[32]="DatosTrabajadores";
+   create_json(&petDriver,archivo);
    HT_Delete( &tabla );
    assert( tabla == NULL );
    //-------------------------------------------------------------------- Creacion del Hash Table
