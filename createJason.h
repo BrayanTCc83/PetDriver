42ce6511743a9970ba9d5017a7c56cef993883f3
GuillermoJR <91043347+GuillermoJR@users.noreply.github.com>
2023-01-12 19:54:15 -0600

    Update createJason.h

--- createJason.h
+++ createJason.h
@@ -5,20 +5,6 @@
 #include <stdlib.h>
 #define MAX_TRABAJADORES 5
 
-/**
- * 
- * @brief Creamos dos estructuras los cuales contienen los datos bases para el funcionamiento de la empresa
- * 
- * @param struct Empleado Contiene las variables donde se almacenaran los datos del empleado.
- * @param id Almacenara el id del empleado.
- * @param nombre Almacenara el nombre del empleado.
- * @param noViajes Almacera el numero de viajes que dara el empleado con el perro.
- * @param tarifa Almacenara la cantidad de la tarifa del empleado.
- * @param struct Empresa Contiene los arreglos donde se almacenaran los datos de la empresa.
- *@param nameEmpresa Almacenara el nombre de la Empresa.
- *@param direccion Almacenara la dirección de la Empresa.
- *@param Empleado empleados Almacenara a los empleados de la Empresa.
-*/
 
 typedef struct Empleado
 {
